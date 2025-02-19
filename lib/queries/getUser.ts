af0b7105f587920be78ba74f24ba10262b16ddd0
Axit <axithhummar4@gmail.com>
2025-02-19 10:54:02 +0530

    cleaned up getUser

--- lib/queries/getUser.ts
+++ lib/queries/getUser.ts
@@ -1,13 +1,17 @@
-import { db } from "@/db";
-import { projects, users } from "@/db/schema";
+import { users } from "@/db/schema";
 import { eq } from "drizzle-orm";
+import { drizzle } from "drizzle-orm/neon-http";
+import * as schema from "../../db/schema";
 
 export async function getUser(email: string) {
-  const user = await db.select().from(users).where(eq(users.email, email));
+  const db = drizzle(process.env.NEXT_PUBLIC_DATABASE_URL!, { schema });
+  const user = await db.query.users.findMany({
+    with: {
+      projects: true,
+    },
+    where: eq(users.email, email),
+  });
   if (!user.length) return null;
-  const userProjects = await db
-    .select({ id: projects.id, name: projects.name })
-    .from(projects)
-    .where(eq(projects.userId, user[0].id));
-  return { ...user[0], projects: userProjects };
+
+  return user[0];
 }
